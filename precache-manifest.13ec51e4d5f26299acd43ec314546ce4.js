161672e94e2680aa085b4485c76023ae559ccbb3
kierepkae <kierepkaedyta@gmail.com>
2019-02-07 21:57:37 +0000

    Updates

---
+++ precache-manifest.13ec51e4d5f26299acd43ec314546ce4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "782ddda302f7c7cb758d",
+    "url": "/todoapp/static/js/main.782ddda3.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/todoapp/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "782ddda302f7c7cb758d",
+    "url": "/todoapp/static/css/main.7f8e679d.chunk.css"
+  },
+  {
+    "revision": "a59a9f54fc1c15ca3feec44b62f9d9bd",
+    "url": "/todoapp/index.html"
+  }
+];
